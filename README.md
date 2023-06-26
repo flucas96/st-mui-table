c0bb614e4abc07079f6c03f79ca58600f642c05d
Fabian Lucas <fabian.lucas@msr.de>
2023-06-26 11:31:03 +0200

    index angepasst readme hinzugefügt

--- README.md
+++ README.md
@@ -1,37 +1,63 @@
-Dies ist eine Vorlage zu erstellung von Streamlit Components.
+# 
 
-Der Names des neuen Component muss über dort wo aktuell "st_ant_carousel" steht geändert werden.
+# Streamlit MUI Table
 
-    MANIFEST.in
-    setup.py
-    st_ant_carousel - Ordner
-    st_ant_carousel/fronted/package.json
+This is a Streamlit custom component that brings Material UI Table component to your Streamlit apps. It allows to show a simple table in the MUI style. There are some parameters that can be used to customize the table. However, by no means this component is a full implementation of the MUI Table component. It is just a simple table that can be used to show data in a nice way. The most advanced features is that "detailColumns" can be defined that are shown when a row gest "expanded". 
 
-    st_ant_carousel/frontend/src/AntCarousel.tsx
-    st_ant_carousel/fronted/src/index.tsx
+Check out the [live demo](https://st-mui-table.streamlit.app/)!
 
+## Installation
 
-node.js muss ebenfall installiert sein. 
-Öffne ein neues Terminal und navigiere in den Ordner st_ant_carousel/fronted
+To install st_mui_table, you can use pip:
 
-    npm install --legacy-peer-deps (immer mit --legacy-peer-deps installieren)
-    npm run start  -> Nun läuft der Frontend Server
+````
+pip install st_mui_table
+````
 
+## Usage
 
-Build
+To use the sst_mui_table component, you just need to import it in your Streamlit script and call it like any other Streamlit function:
 
-    im Frontend Ordner:
+```` python
+from st_mui_table import st_mui_table
+````
+```` python
+st_mui_table(
+    df,
+    enablePagination=True,
+    customCss="",
+    paginationSizes = [5,10,25],
+    size="medium",
+    padding="normal",
+    showHeaders=True,
+    key="mui_table",
+    stickyHeader=True,
+    paperStyle={ "width": '100%',  "overflow": 'hidden',"paddingBottom": '1px', "border": '2px solid rgba(224, 224, 224, 1)'},
+    detailColumns=[],
+    detailColNum=1,
+    detailsHeader="Details",
+    showIndex=False
+    
+)
+````
 
-    npm run build
+## Note worthy
+All HTML content will be correctly interpreted and rendered! Meaning if there is something like `<b>bold</b>` in the dataframe, it will be rendered as bold text. 
+Keep that in mind if users can directly influence the content of the table.
 
-    im Root Ordner:
+## Parameters
 
-    python setup.py bdist_wheel
-
-    Upload zu PyPi:
-
-    twine upload dist/*
-
-
-
-
+- **df**: The dataframe to be shown in the table.
+- **enablePagination**: If True, pagination is enabled. Default: True
+- **customCss**: Custom CSS to be applied to the table. Can just be a string like  `.MuiTableCell-root {color:red;}`  Default: "". The text will be rendered in `<style>` tags above the component.
+- **paginationSizes**: List of integers that define the number of rows per page. Default: [5,10,25] - The first number defined the amount of rows that will be displayed when the table first renders
+- **size**: Size of the table. Can be "small", "medium" or "large". Default: "medium"
+- **padding**: Padding of the table. Can be "normal", "checkbox" or "none". Default: "normal"
+- **showHeaders**: If True, the headers of the table are shown. Default: True
+- **key**: Key of the component. Default: "mui_table"
+- **stickyHeader**: If True, the header of the table is sticky. Default: True
+- **paperStyle**: Dictionary with the style of paper on which the table is on. Default: { "width": '100%',  "overflow": 'hidden',"paddingBottom": '1px', "border": '2px solid rgba(224, 224, 224, 1)'}
+- **detailColumns**: List of columns that will be shown when a row is expanded. Default: [] - All Column in this list will not be displayed in the "normal" table anymore.
+- **detailColNum**: Structure of the columns in the details seciont. If 1 all details will be shown beneath each other. If 2, the details will be shown in two columns. If the 3 details will be shown in three columns. Default: 1...
+- **detailsHeader**: Header of the details section. Default: "Details"
+- **showIndex**: If True, the index of the dataframe should be shown. Default: False
--- app.py
+++ app.py
@@ -6,37 +6,157 @@ import numpy as np
 import pandas as pd
 
 import datetime 
+import random
+from datetime import datetime, timedelta
+
+
 
 st.set_page_config(layout="wide")
 
-
-
-# Create a sample DataFrame
+#Generate random data
+# Set a seed so the random values are the same each time you run the code
 np.random.seed(24)
-df = pd.DataFrame({'A': np.linspace(1, 10, 10)})
-df = pd.concat([df, pd.DataFrame(np.random.randn(10, 4), columns=list('BCDE'))],
-               axis=1)
-df.iloc[0, 2] = "<b>2022-05-06, vollkommen zufrieden </b><br> Der Abschluss der Versicherung lief sehr gut, allerdings habe ich noch keine Erfahrung mit der Schadensregulierung. Auch die Beiträge sind sehr günstig."
-df.iloc[0, 3] = "<b>2022-05-06, vollkommen zufrieden </b><br> Der Abschluss der Versicherung lief sehr gut, allerdings habe ich noch keine Erfahrung mit der Schadensregulierung. Auch die Beiträge sind sehr günstig."
 
-df.rename(columns={'A': '<b>date</b>'}, inplace=True)
+# Generate a list of random dates
+date_today = datetime.now()
+days = pd.date_range(date_today, date_today + timedelta(19), freq='D').strftime('%m/%d/%Y').tolist()
 
-st.title('Streamlit MUI Table Example')
+# Generate a list of product names
+products = ['Product ' + str(i) for i in range(1, 6)]
 
-st.header('Original DataFrame')
-st.dataframe(df)
+# Generate a DataFrame
+df = pd.DataFrame({
+    'Date': days,
+    'Product': [random.choice(products) for _ in range(20)],
+    'Units Sold': [random.randint(1, 100) for _ in range(20)],
+    'Unit Price': [round(random.uniform(10.5, 100.5), 2) for _ in range(20)]
+})
 
-st.header('MUI Table')
-customCss = """
-    .MuiTableContainer-root {
-        border: 1px solid red;
-    }
-"""
 
-col_size = st.slider("Column Size", 6.1, 12.5, 0.5)
-col1, col2 = st.columns((col_size,1))
+# Calculate the Total Sales and add as a new column
+df['Total Sales'] = df['Units Sold'] * df['Unit Price']
+df["Total Sales"] =  df["Total Sales"].round(2)
+st.title('Streamlit Mui Table Component Demo')
+st.divider()
+st.title('Basic Usage')
 
-with col1:
-    st_mui_table(df, enablePagination=True,  paginationSizes = [3,5,10,25], detailColumns=["E","B","D"], detailColNum=2,minRowHeight=1000)
 
-st.write("test")
+
+#gernate random dataframe
+
+
+col_left, col_right = st.columns((1,1))
+
+with col_left:
+
+    st_mui_table(df,key="Table1")
+
+with col_right:
+    st.code("""
+    st_mui_table(df,key="table2")
+    """, language="python")
+
+    st.markdown("<b>Input Dataframe", unsafe_allow_html=True)
+    st.dataframe(df)
+
+
+st.divider()
+
+st.title('HTML Styling in Cells')
+
+st.write("You can use HTML to style the cells")
+
+col_left, col_right = st.columns((1,1))
+
+df2 = df.copy()
+df2['Total Sales'] = df2['Total Sales'].apply(lambda x: f"<b>{x}</b>")
+
+with col_left:
+    st_mui_table(df2,key="table2")
+
+with col_right:
+    st.code("""
+    df2 = df.copy()
+    df2['Total Sales'] = df2['Total Sales'].apply(lambda x: f"<b>{x}</b>")
+    st_mui_table(df2,key="table2")
+    """, language="python")
+
+    st.markdown("<b>Input Dataframe", unsafe_allow_html=True)
+    st.dataframe(df2)
+
+
+st.divider()
+
+st.title('Pagination - Size - Padding - Header')
+
+st.write("You can enable pagination, change the size of the cells, change the padding of the cells and hide the header")
+col_left, col_right = st.columns((1,1))
+
+with col_right:
+
+    pagination = st.checkbox("**Enable Pagination**", value=True)
+    size = st.selectbox("**Size**", ["small", "medium"], index=1)
+    padding = st.selectbox("**Padding**", ["normal", "checkbox", "none"], index=0)
+    showHeaders = st.checkbox("**Show Headers**", value=True)
+    stickyHeader = st.checkbox("**Sticky Header**", value=True)
+
+    st.code(f"""
+    st_mui_table(df2,key="table3", enablePagination={pagination}, size={size}, padding={padding}, showHeaders={showHeaders}, stickyHeader={stickyHeader})
+    """, language="python")
+
+with col_left:
+    st_mui_table(df2,key="table3", enablePagination=pagination, size=size, padding=padding, showHeaders=showHeaders, stickyHeader=stickyHeader)
+
+st.divider()
+
+st.title('Details')
+
+st.write("You can add a details section to each row. You can specify the number of columns to show in the details and the header of the details section")
+
+col_left, col_right = st.columns((1,1))
+
+with col_right:
+    
+        detailColumns = st.multiselect("**Detail Columns**", df.columns, default=["Date", "Product"])
+        detailColNum = st.slider("**Number of Detail Columns**", min_value=0, max_value=len(detailColumns), value=1)
+        detailsHeader = st.text_input("**Details Header**", value="<b>Details</b>")
+        st.code("""
+        for col in detailColumns:
+            df.rename(columns = {col:f"<b>{col}</b>"}, inplace = True)
+        detailColumns = [f"<b>{col}</b>" for col in detailColumns]
+        st_mui_table(df2,key="table4", detailColumns={detailColumns}, detailColNum={detailColNum}, detailsHeader={detailsHeader})
+        """, language="python")
+
+with col_left:
+    for col in detailColumns:
+        df2.rename(columns = {col:f"<b>{col}</b>"}, inplace = True)
+    detailColumns = [f"<b>{col}</b>" for col in detailColumns]
+
+
+    st_mui_table(df2,key="table4", detailColumns=detailColumns, detailColNum=detailColNum, detailsHeader=detailsHeader)
+
+st.divider()
+
+st.title('Custom CSS')
+
+st.write("You can add custom CSS to the table")
+
+col_left, col_right = st.columns((1,1))
+
+with col_right:
+        
+            customCss = st.text_area("**Custom CSS**", value="""
+            .MuiTableCell-root {
+                border: 2px solid green;
+            }
+            """)
+
+            paperCSS  = { "width": '100%',  "overflow": 'hidden',"paddingBottom": '1px', "border": '2px solid red'}
+
+            st.code("""
+            paperCSS  = { "width": '100%',  "overflow": 'hidden',"paddingBottom": '1px', "border": '2px solid red'}
+            st_mui_table(df2,key="table5", customCss={customCss}, paperStyle=paperCSS)
+            """, language="python")
+
+with col_left:
+    st_mui_table(df2,key="table5", customCss=customCss, paperStyle=paperCSS)
